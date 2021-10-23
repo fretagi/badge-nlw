e4416761d201d94cf89a7472b298284270f57c4c
Fernando Retagi <fernando.retagi@gmail.com>
2021-10-23 15:52:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'fretagi',
+  youtube: 'fernando.retagi',
+  facebook: 'fernando.retagi',
+  instagram: 'fretagi',
+  twitter: 'fretagi'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById('userName').textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
